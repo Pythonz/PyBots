0affbc3e5f20e2d2e57a0d408edde6c97317932c
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-02-26 23:23:26 +0100

    all snailbot and little fishbot
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- bots/fishbot.py
+++ bots/fishbot.py
@@ -11,3 +11,12 @@ class fishbot(pybots.Bot):
 	def OnInvite(self, nick, channel):
 		self.join(channel)
 		self.act(channel, "m00s contentedly on "+nick+".")
+
+	def OnChanMsg(self, nick, chan, msg):
+		arg = msg.lower()
+		if arg.find("aftershock") != -1: self.msg(chan, "mmm. Aftershock.")
+		elif arg.startsiwith("fish go"):
+			if arg.split()[2] == "moo" or arg.split()[2] == "mo0" or arg.split()[2] == "m0o" or arg.split()[2] == "m00":
+				self.act(chan, "notes that "+nick+" is truly enlightened.")
+			else:
+				self.msg(chan, nick+" LIES! Fish don't go "+' '.join(msg.split()[2:])+"! fish go m00!")
