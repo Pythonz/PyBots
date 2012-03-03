7fc54cc8f90fb320c58dc6b985777727758a3445
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-03-03 11:31:39 +0100

    make it better
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- pybots.py
+++ pybots.py
@@ -139,16 +139,16 @@ class Bot:
 		os.remove("bots/"+self.uid+"/"+channel+".chan")
 
 if __name__ == "__main__":
-	if len(sys.argv) == 2:
-		while True:
-			print("PyBots (" + __version__ + ") started")
-			PyBots(sys.argv[1]).run()
-			print("PyBots (" + __version__ + ") stopped")
-			time.sleep(5)
-	elif len(sys.argv) == 1:
+	if len(sys.argv) == 1:
 		while True:
 			print("PyBots (" + __version__ + ") started")
 			PyBots("config.cfg").run()
+			print("PyBots (" + __version__ + ") stopped")
+			time.sleep(5)
+	else:
+		while True:
+			print("PyBots (" + __version__ + ") started")
+			PyBots(sys.argv[1]).run()
 			print("PyBots (" + __version__ + ") stopped")
 			time.sleep(5)
 
