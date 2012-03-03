94807bdf8cfeaf800c4bd567521a6603ad11c284
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-03-03 11:22:14 +0100

    maybe its better so
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- pybots.py
+++ pybots.py
@@ -46,36 +46,40 @@ class PyBots:
 					for file in os.listdir("bots/"+self.id+bot_uid):
 						os.remove("bots/"+self.id+bot_uid+"/"+file)
 		while 1:
-			recv = self.con.recv(25600)
-			if not recv:
-				self.con.close()
-				sys.exit(0)
-			for line in recv.splitlines():
-				print("<< "+line)
-				lined = line.split()
-				if len(lined) > 2:
-					if lined[1] == "PING":
-						self.send(":%s PONG %s %s" % (self.id, self.id, lined[2]))
-						self.send(":%s PING %s %s" % (self.id, self.id, lined[2]))
-					if lined[1] == "UID":
-						self.nicklist[lined[2]] = lined[4]
+			try:
+				recv = self.con.recv(25600)
+				if not recv:
+					self.con.close()
+					sys.exit(0)
+				for line in recv.splitlines():
+					print("<< "+line)
+					lined = line.split()
+					if len(lined) > 2:
+						if lined[1] == "PING":
+							self.send(":%s PONG %s %s" % (self.id, self.id, lined[2]))
+							self.send(":%s PING %s %s" % (self.id, self.id, lined[2]))
+						if lined[1] == "UID":
+							self.nicklist[lined[2]] = lined[4]
 
-					if lined[1] == "INVITE":
-						nick = self.nicklist[lined[0][1:]]
-						if self.bots.has_key(lined[2]):
-							exec("""bots.%s.%s("%s", "%s").OnInvite("%s", "%s")""" % (self.bots[lined[2]], self.bots[lined[2]], lined[2], self.conffile, nick, lined[3]))
-					if lined[1] == "IDLE" and len(lined) == 3:
-						if self.bots.has_key(lined[2]):
-							self.send(":"+lined[2]+" IDLE "+lined[0][1:]+" 0 0")
-					if lined[1] == "PRIVMSG":
-						nick = self.nicklist[lined[0][1:]]
-						for bot in self.bots.keys():
-							if os.access("bots/"+bot+"/"+lined[2]+".chan", os.F_OK):
-								text = ' '.join(lined[3:])[1:]
-								if text.startswith("\001ACTION") and text.endswith("\001"):
-									exec("""bots.%s.%s("%s", "%s").OnChanAct("%s", "%s", "%s")""" % (self.bots[bot], self.bots[bot], bot, self.conffile, nick, lined[2], text[8:-1]))
-								else:
-									exec("""bots.%s.%s("%s", "%s").OnChanMsg("%s", "%s", "%s")""" % (self.bots[bot], self.bots[bot], bot, self.conffile, nick, lined[2], text.replace("\"", "\\\"")))
+						if lined[1] == "INVITE":
+							nick = self.nicklist[lined[0][1:]]
+							if self.bots.has_key(lined[2]):
+								exec("""bots.%s.%s("%s", "%s").OnInvite("%s", "%s")""" % (self.bots[lined[2]], self.bots[lined[2]], lined[2], self.conffile, nick, lined[3]))
+						if lined[1] == "IDLE" and len(lined) == 3:
+							if self.bots.has_key(lined[2]):
+								self.send(":"+lined[2]+" IDLE "+lined[0][1:]+" 0 0")
+						if lined[1] == "PRIVMSG":
+							nick = self.nicklist[lined[0][1:]]
+							for bot in self.bots.keys():
+								if os.access("bots/"+bot+"/"+lined[2]+".chan", os.F_OK):
+									text = ' '.join(lined[3:])[1:]
+									if text.startswith("\001ACTION") and text.endswith("\001"):
+										exec("""bots.%s.%s("%s", "%s").OnChanAct("%s", "%s", "%s")""" % (self.bots[bot], self.bots[bot], bot, self.conffile, nick, lined[2], text[8:-1]))
+									else:
+										exec("""bots.%s.%s("%s", "%s").OnChanMsg("%s", "%s", "%s")""" % (self.bots[bot], self.bots[bot], bot, self.conffile, nick, lined[2], text.replace("\"", "\\\"")))
+			except:
+				self.con.close()
+				return 1
 
 
 	def send(self, message):
